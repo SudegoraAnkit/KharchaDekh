d605eda35afae53a1e626029e32f7b47baee2b80
Ankit Sudegora <ankitrai.pro@gmail.com>
2026-06-30 09:27:22 +0530

    chore: bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.ankitsudegora"
     minSdk = 24
     targetSdk = 35
-    versionCode = 6
-    versionName = "1.1.0"
+    versionCode = 7
+    versionName = "1.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
